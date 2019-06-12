4a97df7cd7c52b644392ad5c002275e97b6ff29c
nordcave <nadraga.oleg@gmail.com>
2019-06-13 00:45:42 +0300

    building service and controller

---
+++ src/main/java/dev/nord/springdemo/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package dev.nord.springdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan("dev.nord.springdemo")
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public UrlBasedViewResolver urlBasedViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+}
