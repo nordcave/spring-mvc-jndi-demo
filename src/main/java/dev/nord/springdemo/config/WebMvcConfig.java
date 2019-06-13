22796e74169a9eb6726921574e27adcf02fe726e
nordcave <nadraga.oleg@gmail.com>
2019-06-13 17:09:43 +0300

    JNDI Datasource Configuration

--- src/main/java/dev/nord/springdemo/config/WebMvcConfig.java
+++ src/main/java/dev/nord/springdemo/config/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package dev.nord.springdemo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
@@ -12,6 +15,14 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @ComponentScan("dev.nord.springdemo")
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public DataSource dataSource() {
+		final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+		dsLookup.setResourceRef(true);
+		DataSource dataSource = dsLookup.getDataSource("jdbc/springdb");
+		return dataSource;
+	}
 	
 	@Bean
 	public UrlBasedViewResolver urlBasedViewResolver() {
